8ffc1062ddb863209ec72e01dd751448f99d3bf4
Social777 <douglasrodsil@hotmail.com>
2021-10-16 06:58:18 -0300

    Exercicio-5
    Adicionando uma segunda forma de resolução do problema.

--- Exercicio-5.py
+++ Exercicio-5.py
@@ -3,4 +3,12 @@
 
 num = 55.555
 res = num / 5
-print(f'A quinta parte do referido número é: {res}')
+print(f'\nA quinta parte do referido número é: {res}')
+
+
+# Outra maneira de se fazer.
+
+
+num2 = float(input('\nDigite um número real: '))
+res = num2 / 5.
+print(f'\nO número digitado é {num2} e a quinta parte dele é {res}')
