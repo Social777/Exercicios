ddc3dfe6550408a4678479615b9ce58b83850c1d
Social777 <douglasrodsil@hotmail.com>
2021-10-16 07:39:02 -0300

    Exercicio-6
    Leia uma temperatura em graus celsius e apresente-a convertida em graus Fahrenheit.

---
+++ Exercicio_6.py
@@ -0,0 +1,6 @@
+# Leia uma temperatura em graus celsius e apresente-a convertida em graus Fahrenheit.
+
+
+cel = float(input('\nQual a temperatura em graus celsius? '))
+fah = cel*(9.0/5.0)+32
+print(f'\nA temperatura em Fahrenheit é: {fah}')
