b05084b3fd89f352610eecadffb003c96c60d235
Social777 <douglasrodsil@hotmail.com>
2021-10-15 22:56:16 -0300

    Exercicio-4
    Leia um número real e imprima o resultado do quadrado desse número.

---
+++ Exercicio_4.py
@@ -0,0 +1,9 @@
+# Leia um número real e imprima o resultado do quadrado desse número.
+
+
+num = 4.2
+print(f'\nO Quadrado do número em questão é: {num * num}')
+
+
+# Outra opção de se fazer um número elevado ao quadrado.
+print(f'\nO Quadrado do número em questão é: {num ** 2}')
