5f6a64a9ea4d38917ce3df43378be1c44f055d06
Social777 <douglasrodsil@hotmail.com>
2021-10-16 22:36:14 -0300

    Exercicio-10
    Alteração de float para int.

--- Exercicio_10.py
+++ Exercicio_10.py
@@ -1,6 +1,6 @@
 # Leia uma velocidade km/h(quilômetros por hora) e apresente-a convertida em m/s (metros por segundo).
 
 
-km = float(input('\nQual é a sua velocidade em km/h? '))
+km = int(input('\nQual é a sua velocidade em km/h? '))
 ms = km / 3.6
-print(f'\nA velocidade em m/s(metros por segundo) é: {ms}')
+print(f'\nA velocidade em m/s(metros por segundo) é: {ms.__int__()}')
