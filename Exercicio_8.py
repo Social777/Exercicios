803d9e10f7a82c714ac8e3b4018ecc81a623d000
Social777 <douglasrodsil@hotmail.com>
2021-10-16 22:15:12 -0300

    Exercicio-8
    Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius.

---
+++ Exercicio_8.py
@@ -0,0 +1,6 @@
+# Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius.
+
+
+kel = float(input('\nQual a temperatura em graus Kelvin? '))
+cel = kel - 273.15
+print(f'\nA temperatura em Celsius é: {cel}')
