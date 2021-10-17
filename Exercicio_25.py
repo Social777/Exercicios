8e03f42d450f232b39b67f9c4578d5bcfa3dc05a
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:59:47 -0300

    Create Exercicio_25.py
    Leia um valor de área em Acres e apresente-o convertido em Metros Quadrados.

---
+++ Exercicio_25.py
@@ -0,0 +1,7 @@
+# Leia um valor de área em Acres e apresente-o convertido em Metros Quadrados.
+
+
+ac = float(input('\nDigite a área em Acres: '))
+mq = ac * 4048.58
+
+print(f'\nA área em Metros Quadrados é de: {mq}')
