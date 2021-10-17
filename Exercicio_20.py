633448392be1059179ed13a4224e8331bcef329a
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:33:00 -0300

    Create Exercicio_20.py
    Leia um valor de massa em quilogramas e apresente-o convertido em libras.

---
+++ Exercicio_20.py
@@ -0,0 +1,7 @@
+# Leia um valor de massa em quilogramas e apresente-o convertido em libras.
+
+
+qui = float(input('\nDigite o valor da massa em quilogramas: '))
+lib = qui / 0.45
+
+print(f'\nO valor da massa em Libras é de: {lib}')
