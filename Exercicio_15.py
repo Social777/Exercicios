78e62ddcf71aa0e6c2d2ee68f9ff495c5588faa8
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:19:45 -0300

    Update Exercicio_15.py

--- Exercicio_15.py
+++ Exercicio_15.py
@@ -1,7 +1,7 @@
 # Leia um ângulo em radianos e apresente-o convertido em graus.
 
 
-rad = float(input('Qual é o ângulo? '))
+rad = float(input('\nQual é o ângulo? '))
 gra = rad * 180 / 3.14
 
-print(f'Seu grau convertido e graus é: {gra}')
+print(f'\nSeu grau convertido e graus é: {gra}')
