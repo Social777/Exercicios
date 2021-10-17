2919557cd602936d4dc275a738d0691e8002b933
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:48:15 -0300

    Create Exercicio_23.py
    Leia um valor de comprimento em Metros e apresente-o convertido em Jardas.

---
+++ Exercicio_23.py
@@ -0,0 +1,7 @@
+# Leia um valor de comprimento em Metros e apresente-o convertido em Jardas.
+
+
+mt = int(input('\nDigite o comprimento em Metros: '))
+jar = mt / 0.91
+
+print(f'\nO comprimento em Jardas é de: {jar}')
