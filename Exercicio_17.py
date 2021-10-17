40a3404ec25df05a11b5cbaa7c5e46fc9a2af68f
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:19:57 -0300

    Update Exercicio_17.py

--- Exercicio_17.py
+++ Exercicio_17.py
@@ -1,7 +1,7 @@
 # Leia um valor de comprimento em centímetros e apresente-o convertido em polegadas.
 
 
-cen = float(input('Digite o comprimento em Centímetros: '))
+cen = float(input('\nDigite o comprimento em Centímetros: '))
 pol = cen / 2.54
 
-print(f'Seu comprimento em Polegadas é: {pol}')
+print(f'\nSeu comprimento em Polegadas é: {pol}')
