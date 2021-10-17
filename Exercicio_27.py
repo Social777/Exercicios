273bc06ec1419d7016e221c8fec20dcb30d44591
Social777 <douglasrodsil@hotmail.com>
2021-10-17 15:07:04 -0300

    Create Exercicio_27.py
    Leia um valor de área em Hectares e apresente-o convertido em Metros Quadrados.

---
+++ Exercicio_27.py
@@ -0,0 +1,7 @@
+# Leia um valor de área em Hectares e apresente-o convertido em Metros Quadrados.
+
+
+hec = float(input('\nDigite a área em Hectares: '))
+mq = hec * 10000
+
+print(f'\nA área em Metros Quadrados é de: {mq}')
