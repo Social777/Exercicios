51eaedcc89a7d5cf0f24c20a1708410a9b0270ab
Social777 <douglasrodsil@hotmail.com>
2021-10-17 14:23:02 -0300

    Exercicio-19
    Leia um valor em litros e apresente-o convertido em metros cúbicos m³.

---
+++ Exercicio_19.py
@@ -0,0 +1,7 @@
+# Leia um valor em litros e apresente-o convertido em metros cúbicos m³.
+
+
+lit = float(input('\nDigite o valor em metros cúbicos: '))
+mc = lit / 1000
+
+print(f'\nSeu valor convertido em metros cúbicos m³ é de: {mc}')
