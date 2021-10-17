d45fa2349dc67bb06c1d720d8166852ea4c41cfb
Social777 <douglasrodsil@hotmail.com>
2021-10-17 13:55:37 -0300

    exercicio-14
    Leia um ângulo em graus e apresente-o convertido em radianos.

---
+++ Exercicio_14.py
@@ -0,0 +1,7 @@
+# Leia um ângulo em graus e apresente-o convertido em radianos.
+
+
+ang = float(input('Qual é o ângulo? '))
+rad = ang * 3.14 / 180
+
+print(f'Seu grau convertido e radiano é: {rad}')
