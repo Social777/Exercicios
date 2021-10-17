f8aa381bc78440fc71bb1b0e0c16bf15c60b3dd9
Social777 <douglasrodsil@hotmail.com>
2021-10-17 15:17:59 -0300

    Create Exercicio_28.py
    Faça a leitura de três valores e apresente como resultado a soma dos quadrados dos três valores lidos.

---
+++ Exercicio_28.py
@@ -0,0 +1,10 @@
+# Faça a leitura de três valores e apresente como resultado a soma dos quadrados dos três valores lidos.
+
+
+num1 = float(input('\nDigite o Primeiro valor: '))
+num2 = float(input('Digite o Segundo valor: '))
+num3 = float(input('Digite o Terceiro valor: '))
+
+soma = (num1 ** 2) + (num2 ** 2) + (num3 ** 2)
+
+print(f'\nA soma dos quadrados dos trê valores são: {soma}')
