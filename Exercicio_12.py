9e27b722a3b66fe76e2ec2fe1a08b9bbc35b86bb
Social777 <douglasrodsil@hotmail.com>
2021-10-16 22:45:01 -0300

    Exercicio-12
    Leia uma distância em Milhas e apresente-a convertida em Quilômetros.

---
+++ Exercicio_12.py
@@ -0,0 +1,6 @@
+# Leia uma distância em Milhas e apresente-a convertida em Quilômetros.
+
+
+mi = float(input('\nQual é a distancia em Milhas? '))
+qui = 1.61 * mi
+print(f'\nA distância em Quilômetros é: {qui}')
