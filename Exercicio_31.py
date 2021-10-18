b1c5c018152cbb5c7c64124b9c048580e7f6e4e1
Social777 <douglasrodsil@hotmail.com>
2021-10-18 20:28:56 -0300

    Create Exercicio_31.py
    Leita um número inteiro e imprima o seu antecessor e sucessor.

---
+++ Exercicio_31.py
@@ -0,0 +1,6 @@
+# Leita um número inteiro e imprima o seu antecessor e sucessor.
+
+
+num = int(input('\nDigite um número inteiro: '))
+
+print(f'\nO Antecessor do número é: {num - 1} e o seu Sucessor é: {num + 1}')
