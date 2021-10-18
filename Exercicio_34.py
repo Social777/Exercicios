988b23439d4a6516dc5670d39fab882b213adfbf
Social777 <douglasrodsil@hotmail.com>
2021-10-18 20:53:03 -0300

    Create Exercicio_34.py
    Leia o valor do raio de um círculo e calcule e imprima a área do círculo correspondente.

---
+++ Exercicio_34.py
@@ -0,0 +1,9 @@
+# Leia o valor do raio de um círculo e calcule e imprima a área do círculo correspondente.
+
+
+raio = float(input('\nDigite o valor do Raio: '))
+
+area = 3.141592 * (raio ** 2)
+# area = 3.141592 * (raio ** raio)
+
+print(f'\nA área do circulo é de: {area}')
