442b101ccd7c2f7b33e75d5c6c2a5367f8d89d5d
Social777 <douglasrodsil@hotmail.com>
2021-10-18 20:44:31 -0300

    Create Exercicio_33.py
    Leia o tamanho do lado de um quadrado e imprima como resultado a sua área.

---
+++ Exercicio_33.py
@@ -0,0 +1,8 @@
+# Leia o tamanho do lado de um quadrado e imprima como resultado a sua área.
+
+
+lado = float(input('\nDigite o tamanho do lado do Quadrado: '))
+area = lado ** 2
+# area = lado * lado
+
+print(f'\nA área do seu quadrado é de: {area}')
