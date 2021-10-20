0b8c4855b38ebe1d052424bc99bd77498a67a803
Social777 <douglasrodsil@hotmail.com>
2021-10-19 22:10:00 -0300

    Create Exercicio_39.py
    A importância de R$ 780.000,00 será dividida entre três ganhadores de um concurso. Calcule e imprima a quantia ganha por cada um dos ganhadores.

---
+++ Exercicio_39.py
@@ -0,0 +1,19 @@
+"""
+A importância de R$ 780.000,00 será dividida entre três ganhadores de um concurso. sendo que da quantia total:
+
+-> O primeiro ganhador receberá 46%;
+-> O segundo receberá 32%;
+-> O terceiro receberá o restante;
+
+Calcule e imprima a quantia ganha por cada um dos ganhadores.
+
+"""
+
+
+Primeiro = 780000 * 0.46
+Segundo = 780000 * 0.32
+Terceiro = 780000 * 0.22
+
+print(f'\nO Primeiro ganhador receberá: {Primeiro}')
+print(f'O Segundo ganhador receberá: {Segundo}')
+print(f'O Terceiro ganhador receberá: {Terceiro}')
