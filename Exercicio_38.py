369bce7ab0adace0d28106aa18d365b646ddb59a
Social777 <douglasrodsil@hotmail.com>
2021-10-19 21:13:33 -0300

    Create Exercicio_38.py
    Leia o salário de um funcionário. Calcule e imprima o valor do novo salário, sabendo que ele recebeu um aumento de 25%.

---
+++ Exercicio_38.py
@@ -0,0 +1,8 @@
+# Leia o salário de um funcionário.Calcule e imprima o valor do novo salário, sabendo que ele recebeu um aumento de 25%.
+
+
+Salario = float(input('\nDigite o Salário do funcionário: '))
+
+NovoSalario = Salario * 1.25
+
+print(f'\nO novo Salário do funcionário é de: {NovoSalario}')
