75adc50085d9855cf976fdc678ab9b844fa06c53
Social777 <douglasrodsil@hotmail.com>
2021-10-20 22:48:06 -0300

    Create Exercicio_47.py
    leia um número de 4 dígitos (de 1000 a 9999) e imprima 1 dígito por linha.

---
+++ Exercicio_47.py
@@ -0,0 +1,6 @@
+# leia um número de 4 dígitos (de 1000 a 9999) e imprima 1 dígito por linha.
+
+
+numero = str(input('Digite um número de 4 digitos entre 1000 e 9999: '))
+
+print(f'{numero[0]}\n{numero[1]}\n{numero[2]}\n{numero[3]}')
